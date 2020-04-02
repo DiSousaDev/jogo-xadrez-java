d060048bca3c1c3ea24661f1aa8c698a6b03d491
Diego Sousa <50030046+diegosousa10@users.noreply.github.com>
2020-04-02 20:45:29 -0300

    Posicionando algumas peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,6 +1,8 @@
 package boardgame;
 
 
+import javafx.geometry.Pos;
+
 public class Board {
 
     private int rows;
@@ -33,5 +35,9 @@ public class Board {
     }
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
+    }
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
     }
 }
