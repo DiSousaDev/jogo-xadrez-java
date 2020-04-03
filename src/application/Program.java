aa1c6e96c12f084d22dc5b44dcd0a0f9546cd9f4
Diego Sousa <50030046+diegosousa10@users.noreply.github.com>
2020-04-03 00:40:40 -0300

    Exibindo movimentos possiveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,10 @@ public class Program {
                 System.out.print("Origem: ");
                 ChessPosition source = UI.readChessPosition(scan);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
                 System.out.println();
                 System.out.print("Destino: ");
                 ChessPosition target = UI.readChessPosition(scan);
